1654b5f93ce3db6b34f3ada9607291e6ff6d420e
Yureks Solutions <yureksolutions@gmail.com>
2026-04-08 16:00:17 +0530

    Configure mongoose as external package for server components

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose'],
+  },
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
